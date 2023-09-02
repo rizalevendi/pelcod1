10389b32e59c89beedb5e1b1491992957a400673
rizalevendi <143859968+rizalevendi@users.noreply.github.com>
2023-09-01 19:59:39 -0800

    Add files via upload
    upload file

---
+++ latihan1.py
@@ -0,0 +1,25 @@
+# nama_saya = str(input("masukan nama kamu = "))
+# print(nama_saya, "bertipe", type(nama_saya))
+
+# umur = int(input("masukan umur kamu = "))
+# print(umur,"bertipe", type(umur))
+
+IPK = float(input("masukan IPK kamu  = "))
+print(IPK,"bertipe", type(IPK))
+ 
+# menikah = False
+# print("menikah?",menikah,"bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("matkul : ",matkul_diambil,"bertipe", type(matkul_diambil))
+       
+# matkul_diambil2 = ("web","pemvis")
+
+
+# print(17/3)   
+# print(17%3)
+# print(17*3)
+# print("ho"*3)                      
+
+# nama_universitas="universitas trunojoyo"
+# prnit
